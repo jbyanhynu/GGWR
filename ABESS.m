1080e5d5cd27da2702d3b887c1b177582e79cead
jbyanhynu <52739787+jbyanhynu@users.noreply.github.com>
2025-11-28 21:10:10 +0800

    Update ABESS.m

--- ABESS.m
+++ ABESS.m
@@ -4,6 +4,15 @@ temp=find(zeroPosition==1);
 y(temp)=[];
 x(temp,:)=[];
 n=bw;
+
+ux=mean(x);
+sigmax=std(x);
+uy=mean(y);
+sigmay=std(y);
+x=(x-ux)./sigmax;
+y=(y-uy)./sigmay;
+
+
 [~,p]=size(x);
 Smax=min(floor(n/(log(p)*log(log(n)))),p);
 SICtotal=zeros(Smax,1);
@@ -11,9 +20,15 @@ totalBeta=zeros(Smax,p);
 for s=1:Smax
     [beta,~,As,~]=bess_fixed(y,x,s,n,p);
     totalBeta(s,:)=beta';
-    SIC=n*log((y-x*beta)'*(y-x*beta)/(n-length(As)))+length(As)*log(n)*pentalyCoe;%BIC0Ö¸±ê
+    SIC=n*log((y-x*beta)'*(y-x*beta)/(n-length(As)))+length(As)*log(n)*pentalyCoe;
     SICtotal(s)=SIC;
 end
+
 position=(SICtotal==min(SICtotal));
 finalBeta=totalBeta(position,:);
+
+for k1=1:size(x,2)
+    finalBeta(k1)=finalBeta(k1)*sigmay/sigmax(k1);
+end
+finalBeta=finalBeta';
 end
