8684129e6288cfa89cf865947e47b2fdf650ce3f
Jinbiao Yan <52739787+jbyanhynu@users.noreply.github.com>
2025-12-07 15:41:44 +0800

    Normalize px and py variables in ReadData function
    Added normalization for px and py variables.

--- ReadData.m
+++ ReadData.m
@@ -2,11 +2,16 @@ function [y,px,py,x] = ReadData(path)
 data=xlsread(path,1); %#ok<XLSRD> 
 y=data(:,1);% dependent variable 
 px=data(:,2);% x-axis coordinates
-py=data(:,3);% y-axiscoordinates
-x=data(:,4:6);%independent variables
 
+py=data(:,3);% y-axiscoordinates
+delta=max(max(px)-min(px),max(py)-min(py));%
+px=(px-min(px))./delta;
+py=(py-min(py))./delta;
+x=data(:,4:6);%independent variables
+% y=(y-mean(y))./std(y);
 %% Variable standardization
 % intercept=ones(length(px),1);
 % xx=(x-mean(x))./std(x);
 % x=[intercept,xx];
+
 end
